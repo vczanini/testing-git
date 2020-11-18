1611087c8e795132953c990262737037b09e448c
vczanini <42414244+vczanini@users.noreply.github.com>
2020-11-18 14:36:29 -0300

    major changes
    have done some major changes in here, take a look before I put it in the main

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,92 +1,88 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <ctime>
+using namespace std;
 
 void Image()
-{   std::cout << "\n               )         O_._._._A_._._._O         /(" << std::endl;
-    std::cout << "                `--.___,'=================`.___,--'/" << std::endl;
-    std::cout << "                 `--._.__                 __._,--'/" << std::endl;
-    std::cout << "                     ,. l`~~~~~~~~~~~~~~~'l ,.  /" << std::endl;
-    std::cout << "       __            ||(_)!_!_!_.-._!_!_!(_)||/            __" << std::endl;
-    std::cout << "       `-.__        ||_|____!!_|;|_!!____|_||        __,-'//" << std::endl;
-    std::cout << "            `==---='-----------'='-----------`=---=='    //" << std::endl;
-    std::cout << "        | `--.                                         ,--' |" << std::endl;
-    std::cout << "           ,.`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',.  /" << std::endl;
-    std::cout << "           ||  ____,-------._,-------._,-------.____  ||/" << std::endl;
-    std::cout << "            |||___!`=======!`=======!`=======!___|/||" << std::endl;
-    std::cout << "            || |---||--------||-| | |-!!--------||---| ||" << std::endl;
-    std::cout << "  __O_____O_ll_lO_____O_____O|| |'|'| ||O_____O_____Ol_ll_O_____O__" << std::endl;
-    std::cout << "  o H o o H o o H o o H o o |-----------| o o H o o H o o H o o H o" << std::endl;
-    std::cout << " ___H_____H_____H_____H____O =========== O____H_____H_____H_____H___" << std::endl;
-    std::cout << "                          /|=============| " << std::endl;
-    std::cout << "()______()______()______() '==== +-+ ====' ()______()______()______()" << std::endl;
-    std::cout << "||{_}{_}||{_}{_}||{_}{_}/| ===== |_| ===== |{_}{_}||{_}{_}||{_}{_}||" << std::endl;
-    std::cout << "||      ||      ||     / |==== s(   )s ====|      ||      ||      ||" << std::endl;
-    std::cout << "======================()  =================  ()======================" << std::endl;
-    std::cout << "----------------------/| ------------------- | ----------------------" << std::endl;
-    std::cout << "                     / |---------------------|   " << std::endl;
-    std::cout << "-'--'--'           ()  '---------------------'  ()" << std::endl;
-    std::cout << "                   /| ------------------------- |    --'--'--'" << std::endl;
-    std::cout << "       --'--'     / |---------------------------|    '--'" << std::endl;
-    std::cout << "                ()  |___________________________|  ()           '--'-" << std::endl;
-    std::cout << "  --'-          /| _____________________________| " << std::endl;
-    std::cout << " --' gpyy      / |______________________________| " << std::endl << std::endl;
-}
-void Introduction(int Diff)
 {
-    std::cout << "Wellcome foreign, to the gate number " << Diff << "  of the temple of salazar" << std::endl;
-    std::cout << "You may pass, if you know the password to unlock the gates...\n" << std::endl;
+    std::fstream Input("temple.txt", std::fstream::in);
+    if(Input.is_open())
+    {
+        char ch;
+        std::vector<char> rVector;
+        while(Input >> std::noskipws >> ch) 
+        {
+            rVector.push_back(ch);
+        }
+        for(auto i : rVector)
+        {
+            std::cout << i;
+        }
+        Input.close();
+    }
 }
 
-bool Play(int Diff)
+void Intro(int Diff)
 {
+
+    printf("\n\nWellcome foreign, to the gate numer %d temple of salazar\n", Diff);
+    printf("You may pass, if you know the password to unlock the gates...\n\n");
+}
+
+int Play(int pass, int Diff)
+{
+   
+    int x = rand() % (Diff + 2), y = rand() % (Diff + 2), z = rand() % (Diff+2), Sum, Product, Guess1, Guess2;
+    int Guess3, GuessSum, GuessProduct;
+
     Image();
-    Introduction(Diff);
+    Intro(Diff);
 
-    int x = rand() % (Diff + 2);
-    int y = rand() % (Diff + 2);
-    int z = rand() % (Diff + 2);
-    int Sum = x + y + z;
-    int Product = x*y*z;
-    int Guess1, Guess2, Guess3, GuessSum, GuessProduct;
+    Sum = x + y + z;
+    Product = x*y*z;
 
-    std::cout << "There are 3 numbers\n";
-    std::cout << "They add-up to: " << Sum;
-    std::cout << "\nThey multiplies to: " << Product;
-    std::cout << "\nTell me the the 3 numbers: ";
-    std::cin >> Guess1 >> Guess2 >> Guess3;
-
+    printf("There are 3 numbers\n");
+    printf("They add up to: %d\n", Sum);
+    printf("They multiplies to: %d\n", Product);
+    printf("Tell me the 3 numbers: ");
+    scanf("%d %d %d", &Guess1, &Guess2, &Guess3);
+    
     GuessSum = Guess1 + Guess2 + Guess3;
     GuessProduct = Guess1 * Guess2 * Guess3;
 
-    if (GuessSum == Sum && GuessProduct == Product)
+        if (GuessSum == Sum || GuessProduct == Product)
     {
-        std::cout << ("You can pass this gate now, go foward to the next one\n\n");
-        return true;
+        printf("You can pass the gate now, wellcome to the temple of Salazar\n");
+        pass = 1;
+        return pass;
     }
     else
     {
-        std::cout << ("That's not the code they give to you\n\n");
-        return false;
+        printf("Guards!!! Kill him\n");
+        pass = 0;
+        return pass;
     }
 }
 
 int main()
 {
-    srand(time(NULL)); // Randomize the seed based on the time of the day
-    int LevelDiff = 1;
-    int const Max = 3;
+    srand(time(NULL)); //Randons the seed based on daytime.
+    int LevelDiff = 1, Max = 3, Pass = 0;
 
     while (LevelDiff <= Max)
     {
-        bool bLevelComp = Play(LevelDiff);
-        std::cin.clear();
-        std::cin.ignore();
-
-        if (bLevelComp)
+        if (Pass == 0)
+        {
+           Pass = Play(Pass, LevelDiff);
+        }
+        else
         {
             LevelDiff++;
+            Pass = 0;
         }
         
     }
-    std::cout << "Congratulations, you can enter the temple now";
+    printf("Congratulations, you can enter the temple now");
     return 0;
+}
